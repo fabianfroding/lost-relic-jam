68e930b1cfb1b0060cb6bea4a5cc1991c5eb852b
fabian <fabian.froding@gmail.com>
2022-06-15 18:53:16 +0200

    changed some functionality relating to score, enemies and damage

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,19 +1,13 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class Enemy : MonoBehaviour
 {
-    [SerializeField]
-    Explosive barrel;
-
     public int StartHealth = 100;
     [SerializeField]
     int health;
 
-    public int damage = 5;
     public float distance;
 
     [SerializeField] private int scoreYield = 1;
@@ -34,15 +28,11 @@ public class Enemy : MonoBehaviour
     public UnityEvent<float> OnHealthChange;
     public UnityEvent OnHit;
 
-
-
    // float dist = Vector3.Distance(barrel.position, transform.position);
-
 
     private void Start()
     {
         Health = StartHealth;
-        barrel = GetComponent<Explosive>();
     }
 
     public void KillEnemy()
@@ -51,30 +41,6 @@ public class Enemy : MonoBehaviour
         Destroy(gameObject);
     }
 
-    //NEED HELP!! 
-    public float CalculateDamage()
-    {
-        distance = Vector3.Distance(gameObject.transform.position, barrel.transform.position);
-
-        if (distance < 0.5)
-        {
-            damage = 100;
-        }
-        else if (distance < 1)
-        {
-            damage = 75;
-        }
-        else if (distance < 2)
-        {
-            damage = 50;
-        }
-        else if (distance < 3.5)
-        {
-            damage = 25;
-        }
-
-        return damage;
-    }
     /* public float UpdateHealth(float dist)
      {
          health -= dist;
