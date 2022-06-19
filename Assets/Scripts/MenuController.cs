ba30e05da45e8d44a560d9962b1dfdc1df6e264e
niguelchaos <niguelchaos@gmail.com>
2022-06-19 13:16:47 +0200

    credits in menu

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -5,6 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
+    public GameObject creditsCanvas;
+
     public void StartGame()
     {
         SceneManager.LoadScene("LevelSelect");
@@ -20,5 +22,14 @@ public class MenuController : MonoBehaviour
     {
         PlayerPrefs.SetInt("levelReached", 1);
         SceneManager.LoadScene("LevelSelect");
+    }
+
+    public void OpenCredits()
+    {
+        creditsCanvas.SetActive(true);
+    }
+    public void CloseCredits()
+    {
+        creditsCanvas.SetActive(false);
     }
 }
